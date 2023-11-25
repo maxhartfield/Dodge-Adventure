89f6b950ea3f2df46a890f1025f07c4c79adbdc4
maxhartfield <115897288+maxhartfield@users.noreply.github.com>
2023-11-25 14:23:56 -0600

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,23 +1,34 @@
+/**
+ * Creates, moves, draws, and resets the obstacle.
+ * @author Max Hartfield, Nathan Kim, Amit Kancherla
+ * Collaborators: None
+ * Teacher Name: Bailey
+ * Period: 5
+ * Due Date: 5/19/2022
+ */
+
 import java.awt.*;
-import java.awt.geom.*;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JComponent;
 
+public class Obstacle extends JComponent {
 
-public class Obstacle extends JComponent{
+    int speed;
+    Rectangle rect;
+    private int x;
+    private int y;
+    private int startX;
+    private int startY;
+    static List<Rectangle> boundingRect = new ArrayList<>();
 
-     int speed;
-     Rectangle rect;
-     private int x;
-     private int y;
-
-     private int startX;
-     private int startY;
-     static List<Rectangle> boundingRect = new ArrayList<>();
-
-
-    public Obstacle(Rectangle rect, int speed) {
+    /**
+     * Sets a Rectangle as an obstacle with a given speed.
+     * @param rect the Rectangle to make into an obstacle
+     * @param speed the speed to set the obstacle
+     */
+    public Obstacle(Rectangle rect, int speed)
+    {
     	this.rect = rect;
         this.speed = speed;
         startX = (int)(rect.getX());
@@ -26,6 +37,11 @@ public class Obstacle extends JComponent{
         y = startY;
         boundingRect.add(rect);
     }
+
+    /**
+     * Sets a Rectangle as an obstacle without a speed.
+     * @param rect
+     */
     public Obstacle(Rectangle rect) {
         this.rect = rect;
         this.speed = 0;
@@ -36,29 +52,44 @@ public class Obstacle extends JComponent{
         boundingRect.add(rect);
     }
 
-
+    /**
+     * Changes the size of the obstacle.
+     * @param width the width to set the obstacle
+     * @param height the height to set the obstacle
+     */
     public void changeSize(int width, int height)
     {
         rect.setSize(width, height);
-
     }
+
+    /**
+     * Draws the obstacle.
+     * @param gr the Grpahics to paint on
+     * @param clr the color to set the obstacle
+     */
     public void draw(Graphics2D gr, Color clr)
     {
         gr.setColor(clr);
         gr.fill(rect);
-
     }
+
+    /**
+     * Moves the obstacle a given x and y amount.
+     * @param x moves the obstacle in the x-direction
+     * @param y moves the obstacle in the y-direction
+     */
     public void moveObstacle(int x, int y)
     {
-
         this.x += x;
         this.y += y;
         boundingRect.remove(rect);
         rect.setLocation(this.x, this.y);
         boundingRect.add(rect);
-
     }
 
+    /**
+     * Resets the obstacle to a starting position.
+     */
     public void reset()
     {
         x = startX;
