1490486cc3c5c9ec7bda689652a268a409d49612
maxhartfield <115897288+maxhartfield@users.noreply.github.com>
2023-11-25 14:22:59 -0600

    Update DATester.java

--- DATester.java
+++ DATester.java
@@ -1,26 +1,31 @@
+/**
+ * Runs an instance of GameViewer and displays it in a JFrame.
+ * @author Max Hartfield, Nathan Kim, Amit Kancherla
+ * Collaborators: None
+ * Teacher Name: Bailey
+ * Period: 5
+ * Due Date: 5/19/2022
+ */
+
 import javax.swing.JFrame;
 import javax.swing.JComponent;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.Timer;
 import java.awt.Rectangle;
-import java.awt.geom.Line2D;
 
-public class DATester extends JComponent{
-	private static final int WIDTH = 1500;
-	private static final int HEIGHT = 800;
-	private static final int X_START = 50;
-	private static final int Y_START = HEIGHT / 2 - 50 / 2;
-	private static final int SIDE_LENGTH = 35;
-	private static final int SPEED = 7;
+public class DATester extends JComponent {
+
+	static final int WIDTH = 1500;
+	static final int HEIGHT = 800;
+	static final int X_START = 50;
+	static final int Y_START = HEIGHT / 2 - 50 / 2;
+	static final int SIDE_LENGTH = 35;
+	static final int SPEED = 7;
+	static final String NAME = "Dodge Adventure";
 	
-    public static void main(String[] args) {
-		
-		JFrame frame = new JFrame("Dodge Adventure");
-		GameViewer game = new  GameViewer(new Player(new Rectangle(X_START, Y_START, SIDE_LENGTH, SIDE_LENGTH), SPEED));
+    public static void main(String[] args)
+	{
+		JFrame frame = new JFrame(NAME);
+		GameViewer game = new  GameViewer(new Player
+				(new Rectangle(X_START, Y_START, SIDE_LENGTH, SIDE_LENGTH), SPEED));
 		PlayerMovement pm = new PlayerMovement(game);
 		frame.setSize(WIDTH, HEIGHT);
 		frame.add(new DATester());
